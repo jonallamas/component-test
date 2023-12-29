fb50e5bbf0a5154374c5ddfeb2518814c08ef74e
Jona <llamasjonat@gmail.com>
2023-12-29 16:04:10 -0300

    fix(package/ui): added parserOptions on .eslintrc

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -4,6 +4,10 @@ module.exports = {
   extends: ["@unllamas/eslint-config/react-internal.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    // project: "./tsconfig.lint.json",
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 12,
+    "sourceType": "module"
   },
 };
